4147e7cc446d7a81a4d5f910ce0cd91f7073b88f
QuangDuy <Phanquangduytvt.work@gmail.com>
2025-03-21 17:48:44 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,33 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Register from "./layout/Register";
 import Home from "./layout/Home";
+import { Button } from "./components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<Login />} />
+    //     <Route path="/register" element={<Register />} />
+    //     <Route path="/home" element={<Home />} />
+    //   </Routes>
+    // </Router>
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button variant={"outline"}>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
   );
 }
 
