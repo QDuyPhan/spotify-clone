72651cbb1d328e30745d4e3f6fca7876874e2ef0
QuangDuy <Phanquangduytvt.work@gmail.com>
2025-04-09 15:03:04 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,40 @@ import Login from "./layout/Login";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Register from "./layout/Register";
-import Home from "./layout/Home";
+import Home from "./pages/Home";
 import { Button } from "./components/ui/button";
 import {
+  AuthenticateWithRedirectCallback,
   SignedIn,
   SignedOut,
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import { axiosInstance } from "./lib/axios";
 
 function App() {
+  // const getData = async () => {
+  //   const res = await axiosInstance.get("/users", {
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   });
+  //   console.log(res);
+  // };
   return (
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Login />} />
-    //     <Route path="/register" element={<Register />} />
-    //     <Route path="/home" element={<Home />} />
-    //   </Routes>
-    // </Router>
-    <header>
-      <SignedOut>
-        <SignInButton>
-          <Button variant={"outline"}>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/sso-callback"
+          element={
+            <AuthenticateWithRedirectCallback signUpForceRedirectUrl="/auth-callback" />
+          }
+        ></Route>
+        <Route path="/auth-callback" element={<AuthCallbackPage />}></Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Topbar from "@/components/Topbar";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="">
+      <Topbar></Topbar>
+    </div>
+  );
+};
+
+export default Home;
